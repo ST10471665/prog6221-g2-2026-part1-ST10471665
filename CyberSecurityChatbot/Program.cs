f2628463b7d7d8d68ffc8fa26f96cd687aeb4c4d
noaha <noaharelisky@gmail.com>
2026-03-31 15:44:05 +0200

    Added ASCII art and console UI

--- CyberSecurityChatbot/Program.cs
+++ CyberSecurityChatbot/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CyberSecurityChatbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(@"
+====================================
+ CYBERSECURITY AWARENESS CHATBOT
+====================================
+");
+            Console.ResetColor();
+        }
+    }
+}
